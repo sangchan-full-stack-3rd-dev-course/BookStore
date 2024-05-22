9d38059319177cde6112f7407452152a383a7849
HungKungE <jonsun59@gmail.com>
2024-05-22 20:47:52 +0900

    [users] login API

--- controller/UserController.js
+++ controller/UserController.js
@@ -2,6 +2,9 @@ const connection = require('../db/connect');
 const {
     StatusCodes
 } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const join = (req, res) => {
     const { email, name, password } = req.body;
@@ -9,7 +12,7 @@ const join = (req, res) => {
     let sql = `INSERT INTO users (email, name, password) VALUES(?,?,?)`;
     let values = [email, name, password];
 
-    connection.query(sql, values, (err, result)=>{
+    connection.query(sql, values, (err, results)=>{
         if(err) {
             console.error(err);
             return res.status(StatusCodes.BAD_REQUEST).end();
@@ -23,6 +26,33 @@ const join = (req, res) => {
 
 const login = (req, res) => {
     const { email, password } = req.body;
+
+    let sql = `SELECT * FROM users WHERE email = ?`;
+
+    connection.query(sql, email, (err, results)=>{
+        if(err) {
+            console.error(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        const loginUser = results[0];
+
+        if(loginUser && loginUser.password == password){
+            const token = jwt.sign({ email : loginUser.email },
+                process.env.SECRET_KEY,
+                { expiresIn : '30m', issuer : 'HK' }
+            );
+
+            res.cookie("token", token, { httpOnly : true });
+            res.status(StatusCodes.OK).json({
+                message : `로그인 성공`
+            });
+        } else {
+            res.status(StatusCodes.UNAUTHORIZED).json({
+                message : `로그인 실패!`
+            });
+        }
+    });
 };
 
 const passwordResetRequest = (req, res) => {
