59f189c629f0f244ac2590e485a69373633f9e6c
HungKungE <jonsun59@gmail.com>
2024-05-28 20:32:44 +0900

    [carts] 장바구니 조회 기능 추가

--- controller/CartController.js
+++ controller/CartController.js
@@ -28,22 +28,26 @@ const addToCart = (req, res) => {
 // 장바구니 아이템 목록 조회
 const getCartItems = (req, res) => {
     // cartIds 가 있으면 선택 상품, 없으면 전체 조회
-    const { cartIds } = req.body;
-    
-    // connection.query(sql, category_id, (err, results)=>{
-    //     if(err) {
-    //         console.error(err);
-    //         return res.status(StatusCodes.BAD_REQUEST).end();
-    //     }
+    const { cartIds, user_id } = req.body;
 
-    //     if(results.length) {
-    //         res.status(StatusCodes.OK).json({
-    //             categorys : results
-    //         });
-    //     } else {
-    //         res.status(StatusCodes.NOT_FOUND).end();
-    //     }
-    // });
+    let sql = `SELECT cartItems.id, book_id, title, summary, count, price 
+                FROM cartItems LEFT JOIN books ON cartItems.book_id = books.id
+                WHERE user_id = ?`;
+
+    connection.query(sql, user_id, (err, results)=>{
+        if(err) {
+            console.error(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        if(results.length) {
+            res.status(StatusCodes.OK).json({
+                categorys : results
+            });
+        } else {
+            res.status(StatusCodes.NOT_FOUND).end();
+        }
+    });
 }
 
 // 장바구니 삭제
