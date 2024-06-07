7e7ca0dabdddb6bddfe7ea3c92594b1c1fa3e754
HungKungE <jonsun59@gmail.com>
2024-06-07 19:15:06 +0900

    [carts] 장바구니에 jwt 추가

--- controller/CartController.js
+++ controller/CartController.js
@@ -2,18 +2,33 @@ const connection = require('../db/connect');
 const {
     StatusCodes
 } = require('http-status-codes');
+const {
+    verifyToken
+} = require('../utils/token');
+const { TokenExpiredError, JsonWebTokenError } = require('jsonwebtoken');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 // 장바구니 담기
 const addToCart = (req, res) => {
-    const { book_id, user_id, count } = req.body;
+    const { book_id, count } = req.body;
+
+    let userInfo = verifyToken(req);
+
+    if (userInfo instanceof TokenExpiredError) {
+        console.error("1:",userInfo.message);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    } else if (userInfo instanceof JsonWebTokenError) {
+        console.error("2:",userInfo.message);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    }
     
     let sql = `INSERT INTO cartItems (book_id, user_id, count) VALUES (?,?,?)
                 ON DUPLICATE KEY
                 UPDATE count = count + VALUES(count)`;
 
-    let values = [book_id, user_id, count];
+    let values = [book_id, userInfo.user_id, count];
 
     connection.query(sql, values, (err, results)=>{
         if(err) {
@@ -28,9 +43,19 @@ const addToCart = (req, res) => {
 // 장바구니 아이템 목록 조회
 const getCartItems = (req, res) => {
     // cartIds 가 있으면 선택 상품, 없으면 전체 조회
-    const { cart_ids, user_id } = req.body;
+    const { cart_ids } = req.body;
 
-    let values = [user_id];
+    let userInfo = verifyToken(req);
+
+    if (userInfo instanceof TokenExpiredError) {
+        console.error("1:",userInfo.message);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    } else if (userInfo instanceof JsonWebTokenError) {
+        console.error("2:",userInfo.message);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    let values = [userInfo.user_id];
 
     let sql = `SELECT cartItems.id, book_id, title, summary, count, price 
                 FROM cartItems LEFT JOIN books ON cartItems.book_id = books.id
