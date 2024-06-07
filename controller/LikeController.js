5dcd1d2ab7a8b384738033a8b872d1745241386c
HungKungE <jonsun59@gmail.com>
2024-06-07 18:33:32 +0900

    [likes] 좋아요 추가 시 중복된 값이 들어오면 무시하도록 변경

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -2,14 +2,28 @@ const connection = require('../db/connect');
 const {
     StatusCodes
 } = require('http-status-codes');
+const {
+    verifyToken
+} = require('../utils/token');
 const dotenv = require('dotenv');
+const { JsonWebTokenError, TokenExpiredError } = require('jsonwebtoken');
 dotenv.config();
 
 const addLike = (req, res) => {
-    let { book_id, user_id } = req.body;
+    let { book_id } = req.body;
 
-    let sql = `INSERT INTO likes (user_id, liked_book_id) VALUES (?,?)`;
-    let values = [user_id, book_id];
+    let userInfo = verifyToken(req);
+
+    if (userInfo instanceof TokenExpiredError) {
+        console.error("1:",userInfo.message);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    } else if (userInfo instanceof JsonWebTokenError) {
+        console.error("2:",userInfo.message);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    let sql = `INSERT IGNORE INTO likes (user_id, liked_book_id) VALUES (?,?)`;
+    let values = [userInfo.user_id, book_id];
 
     connection.query(sql, values, (err, results)=>{
         if(err) {
