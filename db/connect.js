c24c2931678e738761bceea03cc391d7c85ff209
HungKungE <jonsun59@gmail.com>
2024-06-14 21:44:01 +0900

    [connect] createPool로 변경

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // .env로 민감한 정보 관리
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 1000,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PSWORD,
+  multipleStatements : true
 });
 
-module.exports = connection;
+module.exports = pool;
